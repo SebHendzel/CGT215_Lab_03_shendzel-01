67a55adb7a98868ba52adf0994be3c5f758a07fa
Sebastian <sebastian.hendzel@gmail.com>
2026-09-02 11:38:31 -0400

    Add project files.

---
+++ CGT215_Lab_03_shendzel-01/CGT215_Lab_03_shendzel-01.cpp
@@ -0,0 +1,51 @@
+// CGT215_Lab_03_shendzel-01.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+void printMenu(int& choice) {
+	cout << "Welcome to my program, please select an operation to perform:" << endl;
+	cout << "1. Multiply" << endl;
+	cout << "2. Division" << endl;
+	cout << "3. Addition" << endl;
+	cout << "\nYour Selection: ";
+	cin >> choice;
+}
+
+void getChoices(float& A, float& B) {
+	cout << "please enter first value>";
+	cin >> A;
+	cout << "please enter second value>";
+	cin >> B;
+}
+
+void firstChoice(float A, float B) {
+	cout << A << " * " << B << " = " << A * B << endl;
+}
+void secondChoice(float A, float B) {
+	cout << A << "/" << B << " = " << A/B << endl;
+}
+void thirdChoice(float A, float B) {
+	cout << A << " + " << B << " = " << A + B << endl;
+}
+int main() {
+	int choice;
+	float A;
+	float B;
+
+	printMenu(choice);
+	getChoices(A, B);
+
+	if (choice == 1) {
+		firstChoice(A, B);
+
+	}
+	if (choice == 2) {
+		secondChoice(A, B);
+
+	}
+	if (choice == 3) {
+		thirdChoice(A, B);
+	}
+		return 0;
+}
